79a77d4e1dd7850350877eff4690f80f2e8373e0
Colin Dawson <me@cjdawson.com>
2019-05-17 14:21:26 +0000

    Merged in feature/LocalServer (pull request #5)
    Feature/LocalServer
    
    * Major refactor.  Switching over to a local server hub style driver allowing multiple programs to control the telescope at one time without the need for the POTH Hub
    
    * Unified the setup dialog
    
    * Implemented shared serial port, Both Telescope and Driver can connect at the same time.
    
    * Ported the focuser implementation from the non server based version.
    
    * Ported the telescope driver code.
    
    * Fixed problem with # not being stripped from the returned string ends. Fixed issue with RA being returned as degress rather than hours.
    
    * Telescope passes validation
    
    * Added a lock around the focuser move.
    
    * Reimplemented CommandBlind and CommandString
    
    * Corrected version information
    
    * Removed the Altitude support as there's a bug in the Autostar and Audiostar firmware

---
+++ FocuserTestConsole/Program.cs
@@ -0,0 +1,55 @@
+﻿// This implements a console application that can be used to test an ASCOM driver
+//
+
+// This is used to define code in the template that is specific to one class implementation
+// unused code can be deleted and this definition removed.
+
+#define Telescope
+// remove this to bypass the code that uses the chooser to select the driver
+#define UseChooser
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using ASCOM.DriverAccess;
+
+namespace ASCOM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Uncomment the code that's required
+#if UseChooser
+            // choose the device
+            string id = ASCOM.DriverAccess.Focuser.Choose("ASCOM.MeadeGeneric.Focuser");
+            if (string.IsNullOrEmpty(id))
+                return;
+            // create this device
+            ASCOM.DriverAccess.Focuser device = new ASCOM.DriverAccess.Focuser(id);
+#else
+            // this can be replaced by this code, it avoids the chooser and creates the driver class directly.
+            ASCOM.DriverAccess.Telescope device = new ASCOM.DriverAccess.Telescope("ASCOM.MeadeGeneric.Telescope");
+#endif
+            // now run some tests, adding code to your driver so that the tests will pass.
+            // these first tests are common to all drivers.
+            Console.WriteLine("name " + device.Name);
+            Console.WriteLine("description " + device.Description);
+            Console.WriteLine("DriverInfo " + device.DriverInfo);
+            Console.WriteLine("driverVersion " + device.DriverVersion);
+
+            // TODO add more code to test the driver.
+            device.Connected = true;
+
+            device.Move(2000);
+            Thread.Sleep(2000);
+            device.Move(-2000);
+
+            device.Connected = false;
+            Console.WriteLine("Press Enter to finish");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Meade.net/SetupDialogForm.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using ASCOM.Utilities;
+using ASCOM.Meade.net;
+
+namespace ASCOM.Meade.net
+{
+    [ComVisible(false)]					// Form not registered for COM!
+    public partial class SetupDialogForm : Form
+    {
+        public SetupDialogForm()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdCancel_Click(object sender, EventArgs e) // Cancel button event handler
+        {
+            Close();
+        }
+
+        private void BrowseToAscom(object sender, EventArgs e) // Click on ASCOM logo event handler
+        {
+            try
+            {
+                System.Diagnostics.Process.Start("http://ascom-standards.org/");
+            }
+            catch (System.ComponentModel.Win32Exception noBrowser)
+            {
+                if (noBrowser.ErrorCode == -2147467259)
+                    MessageBox.Show(noBrowser.Message);
+            }
+            catch (System.Exception other)
+            {
+                MessageBox.Show(other.Message);
+            }
+        }      
+
+        public void SetProfile(ProfileProperties profileProperties)
+        {
+            chkTrace.Checked = profileProperties.TraceLogger;
+            // set the list of com ports to those that are currently available
+            comboBoxComPort.Items.Clear();
+            comboBoxComPort.Items.AddRange(System.IO.Ports.SerialPort.GetPortNames());      // use System.IO because it's static
+            // select the current port if possible
+            if (comboBoxComPort.Items.Contains(profileProperties.ComPort))
+            {
+                comboBoxComPort.SelectedItem = profileProperties.ComPort;
+            }
+        }
+
+        public ProfileProperties GetProfile()
+        {
+            var profileProperties = new ProfileProperties
+            {
+                TraceLogger = chkTrace.Checked,
+                ComPort = comboBoxComPort.SelectedItem.ToString()
+            };
+
+            return profileProperties;
+        }
+    }
+}
