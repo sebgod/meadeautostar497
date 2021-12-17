826f887bdfaa3d05f874c76afe349b33bbfaa7af
Colin Dawson <me@cjdawson.com>
2021-12-17 13:16:20 +0000

    Added mossing file

---
+++ Meade.net.Telescope/ExtensionMethods/DoubelExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace System
+{
+    static class DoubelExtensions
+    {
+        public static bool Between(this double value, double lower, double higher)
+        {
+            return value >= lower & value <= higher;
+        }
+    }
+}
